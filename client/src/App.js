8786e7e1370534c7b52b47d7d2c5d4928e634f6e
Demam20 <59580953+Demam20@users.noreply.github.com>
2020-03-28 20:35:40 -0700

    go

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,24 @@ import React from 'react';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <navbar/>
-      <landing/>
-      <footer/>
+      <Navbar/>
+      <Route exact path = "/" component ={Landing}/>
+      <Route exact path = "/register" component ={Register}/>
+      <Route exact path = "/login" component ={Login}/>
+      <Footer/>
      
     </div>
+    </Router>
   );
 }
 
