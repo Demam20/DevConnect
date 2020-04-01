8db5df2f005ee1801a0295d5a5cb9bca4e31786c
Demam20 <59580953+Demam20@users.noreply.github.com>
2020-03-31 19:29:49 -0700

    react done

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,20 +1,70 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+import calssnames from 'classnames';
 
  class Login extends Component {
+   constructor(){
+     super();
+     this.state = {
+       email:"",
+       password:"",
+       errors:{}
+     };
+     this.onChange = this.onChange.bind(this);
+     this.onSubmit = this.onSubmit.bind(this);
+   }
+   onChange(e) {
+     this.setState({ [e.target.name]: e.target.value });
+   }
+   onSubmit(e){
+     e.preventDefault();
+     const user = {
+       email: this.state.email,
+       password: this.state.password
+     };
+     axios
+        .post("/api/users/login", user)
+        .then(res => console.log(res.data))
+        .catch(err => this.setState({ errors: err.response.data}));
+
+   }
+
   render() {
+    const {errors} = this.state;
     return (
-      <div className="login">
+     <div className="login">
       <div className="container">
         <div className="row">
           <div className="col-md-8 m-auto">
             <h1 className="display-4 text-center">Log In</h1>
             <p className="lead text-center">Sign in to your DevConnector account</p>
-            <form action="dashboard.html">
+            <form onSubmit={this.onSubmit}>
               <div className="form-group">
-                <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" />
-              </div>
+                <input
+                 type="email"
+                 className={calssnames("form-control form-control-lg",{'is-invalid':errors.email})}
+                  placeholder="Email Address"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                   />
+              
+               {errors.email && (
+                 <div className='invalid-feedback'>{errors.email}</div>
+               )}
+               </div>
               <div className="form-group">
-                <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" />
+                <input
+                 type="password"
+                 className={calssnames("form-control form-control-lg",{'is-invalid':errors.password})}
+                  placeholder="Password"
+                   name="password"
+                   value={this.state.password}
+                   onChange={this.onChange} />
+              
+              {errors.password && (
+                 <div className='invalid-feedback'>{errors.password}</div>
+               )}
               </div>
               <input type="submit" className="btn btn-info btn-block mt-4" />
             </form>
