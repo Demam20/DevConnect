a37e6195edd06d2669a4a19b68707d3693158a11
Demam20 <59580953+Demam20@users.noreply.github.com>
2020-03-31 21:03:20 -0700

    first part redux

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,15 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import {Provider} from 'react-redux';
+import store from './store';
 
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <Provider store = {store}>
     <Router>
     <div className="App">
       <Navbar/>
@@ -20,7 +23,9 @@ function App() {
      
     </div>
     </Router>
+    </Provider>
   );
+  
 }
 
 export default App;
