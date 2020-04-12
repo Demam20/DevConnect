31be99ed41d0a81940b9460d1d2fd7c8a8cd3b5c
Demam20 <59580953+Demam20@users.noreply.github.com>
2020-04-11 22:15:46 -0700

    resgister comp

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,10 +1,19 @@
-import {SET_CURRENT_USER} from  './typs';
+import {SET_CURRENT_USER, GET_ERRORS} from  './typs';
+import axios from 'axios';
 
 //Register user action
 
-export const registerUser = (userData) => {
-  return{
-    type : SET_CURRENT_USER,
-    payload : userData
+export const registerUser = (userData,history) => 
+  dispatch => {
+     axios
+         .post('/api/users/register', userData)
+         .then(res => history.push('/login'))
+         .catch(err =>
+             dispatch({
+               type: GET_ERRORS,
+               payload: err.response.data 
+             })
+          );
+  
+  
   }
-}
